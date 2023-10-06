50372b7e4ec100326a507d7c00efa3e45edfef22
Sarão <126261940+SaraoSouz@users.noreply.github.com>
2023-10-06 16:33:48 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Assalariado } from "./model/Assalariado";
 import { comissionado } from "./model/Comissionado";
+import { Horista } from "./model/Horista";
+
 
 const a1 = new Assalariado('Miroslav Klose', '8368725635', 1500);
-
 a1.setNome('washington');
 a1.setCpf('6666666');
 a1.setSalario(2222);
@@ -18,3 +19,12 @@ c1.setTaxaComissao(0.00);
 console.log(`Nome: ${c1.getNome()}, CPF: ${c1.getCpf()}`);
 console.log(`Comissao: ${c1.getTaxaComissao()} Total Vendas: ${c1.getTotalVendas()}`);
 console.log(`Vencimento: ${c1.vencimento()}`);
+
+const h1 = new Horista ('macaco', '9958565337', 40, 69)
+h1.setNome('nananan');
+h1.setCPF(9958565337);
+h1.setValorHora(40);
+h1.setHorasTrabalhadas(8);
+console.log(Nome: ${h1.getNome()}, CPF: ${h1.getCPF()});
+console.log(Valor da Hora: ${h1.getValorHora()}, Horas Trabalhadas: ${h1.getHorasTrabalhadas()});
+console.log(Vencimento: ${h1.vencimento()});
